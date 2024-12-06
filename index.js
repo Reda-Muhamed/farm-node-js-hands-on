17f52ad0719fb64becdef05c0441b12d3a50a2f1
Reda Mohamed <reda.mohamed.reda.201@gmail.com>
2024-12-06 21:41:06 +0200

    remove non functional lines

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 //console.log(dataObj)
-const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
-console.log(slugs);
+//  3rd party package
+// const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+// console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
